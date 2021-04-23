7479e0c4efe05d0da29370c963776d3388ebe734
noxydude <60411452+noxydude@users.noreply.github.com>
2021-04-23 04:19:01 -0400

    can visit other pages

--- web-app/main.py
+++ web-app/main.py
@@ -3,8 +3,8 @@ import os
 import bcrypt
 import jwt
 from dotenv import load_dotenv
-from fastapi import Cookie, FastAPI, HTTPException, Request, Response, status
-from fastapi.responses import JSONResponse
+from fastapi import Cookie, FastAPI, HTTPException, Request, Response, status, Form
+from fastapi.responses import JSONResponse, RedirectResponse
 from fastapi_sqlalchemy import DBSessionMiddleware, db
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -26,8 +26,16 @@ jwt_secret = os.environ["JWT_SECRET"]
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def root(request: Request):
+def get_root_page(request: Request):
     return templates.TemplateResponse('index.html', context={'request': request})
+
+@app.get("/login")
+def get_login_page(request: Request):
+    return templates.TemplateResponse('login.html', context={'request': request})
+
+@app.get("/register")
+def get_register_page(request: Request):
+    return templates.TemplateResponse('register.html', context={'request': request})
 
 def get_session_email(session):
     decoded = jwt.decode(session, jwt_secret, algorithms=["HS256"])
@@ -43,7 +51,6 @@ def post_user(user: UserPassword, response: Response):
         max_age=600,
     )
     return db_user
-
 
 @app.post("/stock", response_model=Stock)
 def post_stock(stock: StockCreate, session: str = Cookie(None)):
@@ -63,7 +70,6 @@ def delete_stock(stock_id, session: str = Cookie(None)):
             detail="Not your stock",
         )
 
-
 @app.post("/user/login")
 def login(user: UserPassword, response: Response):
     email_user = get_user_by_email(db.session, user.email)
@@ -82,6 +88,28 @@ def login(user: UserPassword, response: Response):
         detail="Incorrect email or password",
     )
 
+"""
+@app.post("/user/login")
+def login(response: Response, email: str = Form(...), password: str = Form(...)):
+    email_user = get_user_by_email(db.session, email)
+    if email_user is not None and bcrypt.checkpw(
+        password.encode("utf-8"), email_user.hashed_password.encode("utf-8")
+    ):
+        response.set_cookie(
+            key="session",
+            value=jwt.encode({"email": email}, key=jwt_secret, algorithm="HS256"),
+            max_age=600,
+        )
+        return {"id": email_user.id}
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect email or password",
+    )
+"""
+
+
+
 
 @app.get("/user/logout")
 def logout(response: Response):
@@ -95,14 +123,15 @@ def me(session: str = Cookie(None)):
     return get_user_by_email(db.session, email)
 
 
-logged_in_paths = ["/user/me", "/user/logout"]
+not_logged_in_paths = ["/user", "/user/login", "/login", "/register"]
 
 
 @app.middleware("http")
 async def check_session(request: Request, call_next):
     session = request.cookies.get("session")
-    if session is None and (request.url.path.startswith("/stock") or request.url.path in logged_in_paths):
-        return JSONResponse(status_code=403)
+    if session is None and request.url.path not in not_logged_in_paths:
+        response = RedirectResponse(url='/login')
+        return response
 
     response = await call_next(request)
     if request.url.path != "/user/logout" and session is not None:
